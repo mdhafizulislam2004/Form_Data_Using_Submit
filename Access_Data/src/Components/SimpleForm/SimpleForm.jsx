72a1d4f4af4e65bcf9e8fff3a644dd0cddfc9300
Hafizul Islam <hafizul2004official@gmail.com>
2026-06-07 21:02:40 +0600

    Login Info

--- Access_Data/src/Components/SimpleForm/SimpleForm.jsx
+++ Access_Data/src/Components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,21 @@
 const SimpleForm = () => {
+
+    const submitHendaler = (e) => {
+        e.preventDefault()
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+
+    }
+
     return (
-        <div style={{textAlign:"center"}}>
-            <input type="text"/>
-            <br />
-            <input style={{marginTop:"10px"}} type="submit" value="submit" />
+        <div style={{ textAlign: "center" }}>
+            <form action="" onSubmit={submitHendaler}>
+                <input type="text" placeholder="Enter Your Name" name="name" />
+                <br />
+                <input type="email" name="email" placeholder="Your Email" id="" />
+                <br />
+                <input style={{ marginTop: "10px" }} type="submit" value="submit" />
+            </form>
         </div>
     );
 };
