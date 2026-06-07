617371b7d91eecc7021d2719f5bfeb723ee560b5
Hafizul Islam <hafizul2004official@gmail.com>
2026-06-07 20:39:18 +0600

    Form Data

---
+++ Access_Data/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Explore React Form</h1>
+        </div>
+      <SimpleForm></SimpleForm>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ Access_Data/src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,11 @@
+const SimpleForm = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+            <input type="text"/>
+            <br />
+            <input style={{marginTop:"10px"}} type="submit" value="submit" />
+        </div>
+    );
+};
+
+export default SimpleForm;
