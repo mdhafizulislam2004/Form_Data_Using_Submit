1281a4d4e719ffa2b094c6f75d9b42aada88ee6e
Hafizul Islam <hafizul2004official@gmail.com>
2026-06-13 12:20:42 +0600

    Context Api Learning

--- Access_Data/src/Components/FamilyTree/FamilyTree.jsx
+++ Access_Data/src/Components/FamilyTree/FamilyTree.jsx
@@ -1,11 +1,19 @@
 import Grandpa from "./Grandpa";
 import './FamilyTree.css'
+import { createContext } from "react";
+
+export const assestContext=createContext('')
 
 const FamilyTree = () => {
+
+    const newAssest="Diamond";
+
     return (
         <div className="family-tree">
             <h1>Family Tree</h1>
-            <Grandpa></Grandpa>
+            <assestContext.Provider value={newAssest}>
+                <Grandpa></Grandpa>
+            </assestContext.Provider>
         </div>
     );
 };
--- Access_Data/src/Components/FamilyTree/Special.jsx
+++ Access_Data/src/Components/FamilyTree/Special.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { assestContext } from "./FamilyTree";
+
 const Special = () => {
+
+    const newAssest=useContext(assestContext)
+
     return (
         <div>
             <h3>Special Section</h3>
+            <h5>Assests:{newAssest}</h5>
         </div>
     );
 };
